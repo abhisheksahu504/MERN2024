6555d3b1c5a5faa204ca22ee0fd5d1f941670a74
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-04-12 15:56:43 +0530

    working on logic and signup logics

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 //Home Logic
 const home = async (req, res) => {
@@ -14,15 +15,44 @@ const register = async (req, res) => {
   try {
     const { username, email, phone, password } = req.body;
 
-    const userExist = User.findOne({ email: email });
+    const userExist = await User.findOne({ email: email });
+    //const userExist = await User.findOne({ email }); same same but different
     //msg if user is already registered
     if (userExist) {
       return res.status(400).json({ msg: "email already exist" });
     }
-    await User.create({ username, email, phone, password });
-    res.status(200).json({ data });
+    //hash the password
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound);
+
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      // password: hash_password,
+      password,
+    });
+    res.status(201).json({
+      msg: "Registration succesfull",
+      token: await userCreated.generateToken(),
+      userId: userCreated._id.toString(),
+    });
   } catch (error) {
     res.status(400).json("internal server error");
   }
 };
-module.exports = { home, register };
+
+//User Login Logic
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json("Internal server error");
+  }
+};
+module.exports = { home, register, login };
--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const router = require("./router/auth-route");
 const connectDB = require("./utils/db");
 
 app.use(express.json());
+
 //mount the router
 app.use("/api/auth", router);
+
+//All this code is transferred to auth rout and auth controller files
 // app.get("/", (req, res) => {
 //   res.status(200).send("Welcome");
 // });
 // app.get("/register", (req, res) => {
 //   res.status(200).send("Registration Page");
 // });
+
 const PORT = 5000;
 connectDB().then(() => {
   app.listen(PORT, () => {
