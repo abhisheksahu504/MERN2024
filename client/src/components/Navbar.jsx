f0b4b4f9eb882b232810593c898c63fedf3c86e4
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-04-17 11:16:44 +0530

    login added to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,6 +25,9 @@ export const Navbar = () => {
                 <NavLink to="/contact">Contact</NavLink>
               </li>
               <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
                 <NavLink to="/register">Register</NavLink>
               </li>
             </ul>
