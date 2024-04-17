3221a75b8f1f5fc99cd35d3aa4e71e534f67ba29
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-04-17 15:53:54 +0530

    contact page done all css done

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,101 @@
-import React from "react";
+import { useState } from "react";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+  //handling the input fields
+  const handleInput = (e) => {
+    console.log(e);
+    let name = e.target.name;
+    let value = e.target.value;
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contact);
+  };
+  return (
+    <>
+      <section className="section-contact">
+        <div className="conatact-content container">
+          <h1 className="main-heading">Contact Us</h1>
+        </div>
+        <div className="container grid grid-two-cols">
+          {/* image section  */}
+          <div>
+            <div className="contact-image">
+              <img
+                src="/images/support.png"
+                alt="ready to help always"
+                width="500 px"
+                height="500px"
+              />
+            </div>
+          </div>
+          {/* form section  */}
+          <div className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  id="username"
+                  required
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  id="email"
+                  required
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  autoComplete="off"
+                ></textarea>
+              </div>
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </div>
+        <section className="mb-3">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2613173278896!2d73.91411937501422!3d18.562253982539413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1697604225432!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </section>
+      </section>
+    </>
+  );
 };
