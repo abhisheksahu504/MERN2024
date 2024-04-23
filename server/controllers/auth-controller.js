4424427865d4fbfa0a33e92dab212f2516b61152
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-04-23 12:07:32 +0530

    auth-middleware created to verify jwt tokens

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -73,4 +73,14 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { home, register, login };
+//To send user data - User logic
+const user = async (req, res) => {
+  try {
+    const userData = req.user;
+    console.log(userData);
+    return res.status(200).json({ msg: userData });
+  } catch (error) {
+    console.log(`error from the user routes ${error}`);
+  }
+};
+module.exports = { home, register, login, user };
--- server/router/auth-route.js
+++ server/router/auth-route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 const { loginSchema, signupSchema } = require("../validators/auth-validator");
 const validate = require("../middlewares/validate-middleware");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 router.route("/").get(authcontrollers.home);
 router
@@ -10,4 +11,6 @@ router
   .post(validate(signupSchema), authcontrollers.register);
 router.route("/login").post(validate(loginSchema), authcontrollers.login);
 
+//getting datas from data base for autofill
+router.route("/user").get(authMiddleware, authcontrollers.user);
 module.exports = router;
