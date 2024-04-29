4b6f626a006f61dcad93827a007c6149401b0bdd
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-04-29 19:05:30 +0530

    jwt verification in admin route

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const adminContoller = require("../controllers/admin-controller");
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
-router.route("/users").get(adminContoller.getAllUsers);
-router.route("/contacts").get(adminContoller.getAllContacts);
+router.route("/users").get(authMiddleware, adminContoller.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminContoller.getAllContacts);
 module.exports = router;
