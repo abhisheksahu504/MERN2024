f3e92964e9631c666f1690e980119f45dfc37908
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-05-03 18:01:08 +0530

    admin now fetch dtata of users and css added

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -6,6 +6,7 @@ export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState("");
   const [services, setServices] = useState("");
+  const authorizationToken = `Bearer ${token}`;
   const storeTokenInLS = (serverToken) => {
     //no need to refesh after a login
     setToken(serverToken);
@@ -24,7 +25,7 @@ export const AuthProvider = ({ children }) => {
       const response = await fetch("http://localhost:5000/api/auth/user", {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: authorizationToken,
         },
       });
       if (response.ok) {
@@ -58,7 +59,14 @@ export const AuthProvider = ({ children }) => {
   }, []);
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, storeTokenInLS, logoutUser, user, services }}
+      value={{
+        isLoggedIn,
+        storeTokenInLS,
+        logoutUser,
+        user,
+        services,
+        authorizationToken,
+      }}
     >
       {children}
     </AuthContext.Provider>
